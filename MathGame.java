9eafe6f7e93c76f7cc9a1bf78fa5f5107361d2fd
wanko <kouji124@w4.dion.ne.jp>
2020-10-10 03:01:48 +0900

    5th ish try

--- MathGame.java
+++ MathGame.java
@@ -9,47 +9,55 @@ public class MathGame {
 
         System.out.println("Hello and welcome to the two player math game, would you like to play? (y/n)");
 
-        char userChoice = input.nextChar();
+        String userChoice = input.nextLine();
 
         int player1Lives = 3;
         int player2Lives = 3;
-        int num1 = (int)(Math.random() * 101);
-        int num2 = (int)(Math.random() * 101);
         int answer = -1;
+        int playerNum = 1;
 
 
-        if (userChoice == "Y" || userChoice == "y") {
-        
+        if (userChoice.equals("Y") || userChoice.equals("y")) {
+            System.out.println("You each have 3 lives");
+            
             do {
-                // Player 1 to Player 2
-                System.out.println("You each have 3 lives");
-                System.out.println("Player 1: what is " + num1 + " + " + num2);
-                answer = input.nextInt();
-                if (answer == num1 + num2) {
-                    System.out.println("Correct!");
-                } else {
-                    player2Lives--;
+                int num1 = (int)(Math.random() * 101);
+                int num2 = (int)(Math.random() * 101);
+                
+                if (playerNum == 1) {
+                    // Player 1 to Player 2
+                    System.out.println("Player 1: what is " + num1 + " + " + num2);
+                    answer = input.nextInt();
+                    if (answer == num1 + num2) {
+                        System.out.println("Correct!");
+                    } else {
+                        System.out.println("Incorrect! The answer was " + (num1 + num2));
+                        player2Lives--;
+                    }
+                    System.out.println("The lives: Player 1: " + player1Lives + "/3 Player 2: " + player2Lives + "/3");
+                    playerNum = 2;
+                } else if (playerNum == 2) {
+                    // Player 2 to Player 1
+                    num1 = (int)(Math.random() * 101);
+                    num2 = (int)(Math.random() * 101);
+                    System.out.println("Player 2: what is " + num1 + " + " + num2);
+                    answer = input.nextInt();
+                    if (answer == num1 + num2) {
+                        System.out.println("Correct!");
+                    } else {
+                        System.out.println("Incorrect! The answer was " + (num1 + num2));
+                        player1Lives--;
+                    }
+                    System.out.println("The lives: Player 1: " + player1Lives + "/3 Player 2: " + player2Lives + "/3");
+                    playerNum = 1;
                 }
-                System.out.println("The lives: Player 1: " + player1Lives + "/3 Player 2: " + player2Lives + "/3");
-
-                // Player 2 to Player 1
-                System.out.println("Player 2: what is " + num1 + " + " + num2);
-                answer = input.nextInt();
-                if (answer == num1 + num2) {
-                    System.out.println("Correct!");
-                } else {
-                    player1Lives--;
-                }
-                System.out.println("The lives: Player 1: " + player1Lives + "/3 Player 2: " + player2Lives + "/3");
-
-            } while(player1Lives > 0 || player2Lives > 0);
+            } while(player1Lives > 0 && player2Lives > 0);
 
             if (player1Lives == 0) {
-                System.out.println("Player 1 wins!");
-            } else {
                 System.out.println("Player 2 wins!");
+            } else {
+                System.out.println("Player 1 wins!");
             }
-
         }
 
     }
